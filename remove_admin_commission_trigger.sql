4fc65143fc54c1c14e327c9d76aa96249b26c8ff
Big Mike <mdkeener@gmail.com>
2025-10-22 12:00:47 -0600

    file clean up

--- remove_admin_commission_trigger.sql
+++
@@ -1,7 +0,0 @@
--- Remove the admin commission prevention trigger entirely
--- We want to track admin commissions, just not pay them out
-
-DROP TRIGGER IF EXISTS trigger_prevent_admin_commission ON commissions CASCADE;
-DROP FUNCTION IF EXISTS prevent_admin_commission() CASCADE;
-
-SELECT 'Admin commission trigger removed. All commissions will now be tracked regardless of role.' AS status;
